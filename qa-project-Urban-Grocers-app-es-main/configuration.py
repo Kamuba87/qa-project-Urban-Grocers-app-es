406653aef75579ae7a4a4372d037b8917f910347
Karina Muñoz <cokamuba12@gmail.com>
2024-10-30 20:28:37 -0600

    prueba

--- qa-project-Urban-Grocers-app-es-main/configuration.py
+++ qa-project-Urban-Grocers-app-es-main/configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-e44d8a71-0026-47ac-a64d-d78ae691cfae.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-97765fae-c2ff-428c-bd9c-7ba4986634a5.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH =  "/api/v1/users/" # Almacena la ruta para crear un usuario o usuaria en esta variable
 CREATE_KITS_PATH = "/api/v1/kits/" # Almacena la ruta para crear un kit en esta variable
